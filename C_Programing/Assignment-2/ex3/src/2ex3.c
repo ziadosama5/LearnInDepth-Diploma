40b0baa4e02231a064d9a150f1181352e73823cd
Ziad Osama <78916792+Ziad-phsyco@users.noreply.github.com>
2023-05-02 20:22:26 +0300

    tests

--- C_Programing/Assignment-2/ex3/src/2ex3.c
+++ C_Programing/Assignment-2/ex3/src/2ex3.c
@@ -14,15 +14,19 @@
 int main(void) {
 	float a ,b, c;
 	printf("Enter three numbers: ");
-	fflush(stdin); fflush(stdout);
 	scanf("%f %f %f", &a,&b,&c);
 	if (a>=b && a>=c){
-		printf("Largest number %.2f",a);
+		printf("Largest number %.2f \n",a);
+		system("pause");
 	}
 	else if (b>=c && b>=a){
-			printf("Largest number %.2f",b);
+			printf("Largest number %.2f \n",b);
+			system("pause");
 		}
 	if (c>=a && c>=b){
-			printf("Largest number %.2f",c);
+			printf("Largest number %.2f \n",c);
+			system("pause");
 		}
+	return 0;
+	
 }
